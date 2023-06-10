6605f9347279be4480bc3509ca82cc9a5e511c67
Behn <7383025+BehnH@users.noreply.github.com>
2023-06-10 20:56:28 +0100

    Support `excludeExpired` query param

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,22 +1,35 @@
-import lookupCerts from "@/lib/postgres/cert";
+import { lookupCerts, lookupCertsWithoutExpired } from "@/lib/postgres/cert";
 import { getCache } from "@/lib/redis/redis";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const domain = searchParams.get('domain');
+  const excludeExpired = searchParams.get('excludeExpired');
   const cache = await getCache();
 
   if (domain) {
-    const cached = await cache.get(domain);
+    if (excludeExpired) {
+      const cached = await cache.get(`non-expired-${domain}`);
 
-    if (cached) {
-      console.log('Cache hit for', domain);
-      console.log(cached)
-      return NextResponse.json(JSON.parse(cached));
+      if (cached) {
+        console.log('Cache hit for', domain);
+        return NextResponse.json(JSON.parse(cached));
+      } else {
+        const response = await lookupCertsWithoutExpired(domain, false);
+        return NextResponse.json(response);
+      }
+
     } else {
-      const response = await lookupCerts(domain);
-      return NextResponse.json(response);
+      const cached = await cache.get(`all-${domain}`);
+
+      if (cached) {
+        console.log('Cache hit for', domain);
+        return NextResponse.json(JSON.parse(cached));
+      } else {
+        const response = await lookupCerts(domain, false);
+        return NextResponse.json(response);
+      }
     }
   } else {
     return NextResponse.json({ error: 'No domain provided' });
